332fc496301fdc2d6520c64e5683ba1b5cfc6f29
David Blackwelder <github@davidblackwelder.com>
2020-08-14 22:01:50 -0400

    Setup uuidv4 library to assign unique IDs to each task

--- tasks-app.js
+++ tasks-app.js
@@ -1,31 +1,32 @@
 const tasks = getSavedTasks();
 
 const filters = {
-  searchText: "",
-  hideCompleted: false,
+	searchText: "",
+	hideCompleted: false,
 };
 
 renderTasks(tasks, filters);
 
 document.querySelector("#search-text").addEventListener("input", function (e) {
-  filters.searchText = e.target.value;
-  renderTasks(tasks, filters);
+	filters.searchText = e.target.value;
+	renderTasks(tasks, filters);
 });
 
 document.querySelector("#create-task").addEventListener("submit", function (e) {
-  e.preventDefault();
-  tasks.push({
-    text: e.target.elements.newTask.value,
-    completed: false,
-  });
-  saveTasks(tasks);
-  renderTasks(tasks, filters);
-  e.target.elements.newTask.value = "";
+	e.preventDefault();
+	tasks.push({
+		id: uuidv4(),
+		text: e.target.elements.newTask.value,
+		completed: false,
+	});
+	saveTasks(tasks);
+	renderTasks(tasks, filters);
+	e.target.elements.newTask.value = "";
 });
 
 document
-  .querySelector("#hide-completed")
-  .addEventListener("change", function (e) {
-    filters.hideCompleted = e.target.checked;
-    renderTasks(tasks, filters);
-  });
+	.querySelector("#hide-completed")
+	.addEventListener("change", function (e) {
+		filters.hideCompleted = e.target.checked;
+		renderTasks(tasks, filters);
+	});
