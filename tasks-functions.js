42db68ff9398cb68dda5cabf963cc3a2b89f16e4
David Blackwelder <github@davidblackwelder.com>
2020-08-14 22:18:57 -0400

    Add ability to remove tasks by their ID

--- tasks-functions.js
+++ tasks-functions.js
@@ -1,70 +1,85 @@
 // Read existing notes from localStorage
 const getSavedTasks = function () {
-  const tasksJSON = localStorage.getItem("tasks");
+	const tasksJSON = localStorage.getItem("tasks");
 
-  if (tasksJSON !== null) {
-    return JSON.parse(tasksJSON);
-  } else {
-    return [];
-  }
+	if (tasksJSON !== null) {
+		return JSON.parse(tasksJSON);
+	} else {
+		return [];
+	}
 };
 
 // Save tasks to localStorage
 const saveTasks = function (tasks) {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+const removeTask = function (id) {
+	const taskIndex = tasks.findIndex(function (task) {
+		return task.id === id;
+	});
+
+	if (taskIndex > -1) {
+		tasks.splice(taskIndex, 1);
+	}
 };
 
 // Render application tasks based on filters
 const renderTasks = function (tasks, filters) {
-  const filteredTasks = tasks.filter(function (task) {
-    const searchTextMatch = task.text
-      .toLowerCase()
-      .includes(filters.searchText.toLowerCase());
-    const hideCompletedMatch = !filters.hideCompleted || !task.completed;
+	const filteredTasks = tasks.filter(function (task) {
+		const searchTextMatch = task.text
+			.toLowerCase()
+			.includes(filters.searchText.toLowerCase());
+		const hideCompletedMatch = !filters.hideCompleted || !task.completed;
 
-    return searchTextMatch && hideCompletedMatch;
-  });
+		return searchTextMatch && hideCompletedMatch;
+	});
 
-  const incompleteTasks = filteredTasks.filter(function (task) {
-    return !task.completed;
-  });
+	const incompleteTasks = filteredTasks.filter(function (task) {
+		return !task.completed;
+	});
 
-  document.querySelector("#tasks").innerHTML = "";
+	document.querySelector("#tasks").innerHTML = "";
 
-  document
-    .querySelector("#tasks")
-    .appendChild(generateSummaryDOM(incompleteTasks));
+	document
+		.querySelector("#tasks")
+		.appendChild(generateSummaryDOM(incompleteTasks));
 
-  filteredTasks.forEach(function (task) {
-    document.querySelector("#tasks").appendChild(generateTaskDOM(task));
-  });
+	filteredTasks.forEach(function (task) {
+		document.querySelector("#tasks").appendChild(generateTaskDOM(task));
+	});
 };
 
 // Get the DOM elements for an individual task
 const generateTaskDOM = function (task) {
-  const taskEl = document.createElement("div");
-  const checkBox = document.createElement("input");
-  const taskTextEl = document.createElement("span");
-  const button = document.createElement("button");
+	const taskEl = document.createElement("div");
+	const checkBox = document.createElement("input");
+	const taskTextEl = document.createElement("span");
+	const button = document.createElement("button");
 
-  // Setup task checkbox
-  checkBox.setAttribute("type", "checkbox");
-  taskEl.appendChild(checkBox);
+	// Setup task checkbox
+	checkBox.setAttribute("type", "checkbox");
+	taskEl.appendChild(checkBox);
 
-  // Setup the task text
-  taskTextEl.textContent = task.text;
-  taskEl.appendChild(taskTextEl);
+	// Setup the task text
+	taskTextEl.textContent = task.text;
+	taskEl.appendChild(taskTextEl);
 
-  // Setup the remove task button
-  button.textContent = "x";
-  taskEl.appendChild(button);
+	// Setup the remove task button
+	button.textContent = "x";
+	taskEl.appendChild(button);
+	button.addEventListener("click", function () {
+		removeTask(task.id);
+		saveTasks(tasks);
+		renderTasks(tasks, filters);
+	});
 
-  return taskEl;
+	return taskEl;
 };
 
 // Get the DOM elements for list summary
 const generateSummaryDOM = function (incompleteTasks) {
-  const summary = document.createElement("h2");
-  summary.textContent = `You have ${incompleteTasks.length} tasks left to complete`;
-  return summary;
+	const summary = document.createElement("h2");
+	summary.textContent = `You have ${incompleteTasks.length} tasks left to complete`;
+	return summary;
 };
